637a392597466a6a3dc8fcb1b762928a745d4c77
liquidsword <audenrobertson@gmail.com>
2018-07-25 19:19:27 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root "static_pages#home"
 get "/signin", to: "sessions#new"
-post "/sessions/create", to: "sessions#create"
+post "/sessions/create", to: "sessions#create", as: 'register'
 delete "/signout", to: "sessions#destroy"
 
 resources :recipes
