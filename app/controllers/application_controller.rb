c0be09b8078def74eb305dc2c3b5cfe891e6234c
liquidsword <audenrobertson@gmail.com>
2018-07-25 22:55:59 -0400

    updated application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  configure do
-    enable :sessions
-    set :session_secret, "secret"
+  before_action :current_user
+
+  before_action :require_logged_in, except: [:new, :create, :home]
+
+  def logged_in?
+    !!current_user
   end
+
+  private
+    def require_logged_in
+      redirect_to root_path unless logged_in?
+    end
+
+    def current_user
+      @current_user ||= Culinary_artist(session[:culinary_artist_id]) if session[:culinary_artist_id]
+    end
 end
