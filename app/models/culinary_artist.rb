1ca5437017ffab36c70578510f8cecab3377f63b
liquidsword <audenrobertson@gmail.com>
2018-09-30 22:51:04 -0400

    defined password

--- app/models/culinary_artist.rb
+++ app/models/culinary_artist.rb
@@ -4,4 +4,14 @@ class CulinaryArtist < ApplicationRecord
   has_secure_password
   has_many :recipes
   has_many :ingredients, through: :recipes
+
+  def password
+    @password ||= Password.new(password_digest)
+  end
+
+  def password = (new_password)
+    @password = Password.create(new_password)
+    self.password_digest = @password
+
+  end
 end
