22c7ebdc3b70d8fd1051bb036374dcb5e5009589
liquidsword <audenrobertson@gmail.com>
2018-10-07 14:25:07 -0400

    changed config to autoload

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,8 @@ Bundler.require(*Rails.groups)
 module MyRecipes
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    #config.load_defaults 5.2 (removed this code on 10-7)
+    config.autoload_paths += %W(#{config.root}/lib)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
