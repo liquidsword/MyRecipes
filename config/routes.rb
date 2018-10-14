d69add44b63c7b67a5447e7832721c88c497fe26
liquidsword <audenrobertson@gmail.com>
2018-10-14 17:18:07 -0400

    updating views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root "home_page#home"
+root to: "home_page#home"
 get "/signin", to: "sessions#new"
 post "/signin", to: "sessions#create"
 get '/auth/facebook/callback' => 'sessions#omnicreate'
