627d08f0b1b3028c84a2998c8f3e7a572509d3da
liquidsword <audenrobertson@gmail.com>
2018-11-15 13:09:40 -0500

    deleting unused code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  #before_action :require_login #except: [:new, :create, :home]
-
 private
 
   def current_user
@@ -11,19 +9,10 @@ private
 
   def require_login
     redirect_to signin_path, :notice => "Please sign in" unless logged_in?
-
   end
 
   def logged_in?
     current_user ? true : false
-  end
-
-  def redirect_if_logged_in
-    redirect_to root_path if logged_in?
-  end
-
-  def redirect_if_not_logged_in
-    redirect_to signin_path unless logged_in?
   end
 
   helper_method :current_user, :logged_in?
