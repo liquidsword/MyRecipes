9914f7b2a707f63bc6d5d242f882695eb33bcb17
liquidsword <audenrobertson@gmail.com>
2018-11-20 22:10:26 -0500

    updating Homepage

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,5 +1,8 @@
 class HomePageController < ApplicationController
+  def show
 
+  end
+  
   def home
     if session[:culinary_artist_id]
       @culinary_artist = CulinaryArtist.find_by(session[:email])
