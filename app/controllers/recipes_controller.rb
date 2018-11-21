448ce45d7caa6c2d159711f039bc7e00c301f29e
liquidsword <audenrobertson@gmail.com>
2018-11-20 22:06:42 -0500

    deleted binding pry

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -19,7 +19,6 @@ class RecipesController < ApplicationController
   end
 
   def create
-    binding.pry
     @recipe = Recipe.create(recipe_params)
     @recipe.culinary_artist_id = current_user
     if @recipe.valid?
