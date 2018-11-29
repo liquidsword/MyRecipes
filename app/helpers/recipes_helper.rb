964ef1a3cb2c774dd4423979096736efe212cf0b
liquidsword <audenrobertson@gmail.com>
2018-11-28 23:23:45 -0500

    updating recipe form
    updating recipe form and helper to make sure recipe_ingredient table will be updated

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,10 @@
 module RecipesHelper
+  def culinary_artist_id_field(recipe)
+       if @recipe.culinary_artist.nil?
+         select_tag "recipe[culinary_artist_id]",
+          options_from_collection_for_select(CulinaryArtist.all, :id, :culinary_artist_name)
+       else
+        hidden_field_tag "recipe[culinary_artist_id]", recipe.culinary_artist_id
+      end
+    end
 end
